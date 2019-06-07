1a50523ccc0d094691cb87d4dd5642cc58da3c09
vphson <vophuochoangson@gmail.com>
2019-06-08 00:20:45 +0700

    res.locals

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }))
  
 
 app.use(express.json())
+app.use(require('./middleware/category.mdw'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
--- models/category.model.js
+++ models/category.model.js
@@ -3,7 +3,11 @@ var db = require('../utils/db');
 module.exports = {
     all:()=>{
        
-        return db.load('select * from categories1');
+        return db.load('select * from categories');
+    },
+    allWithDetails:()=>{
+       
+        return db.load('SELECT c.*, COUNT(n.NewsID) AS num_of_news FROM categories c LEFT JOIN NEWS n on c.CatID = n.CatID GROUP by c.CatID, c.CatName');
     },
     single: id => {
        
